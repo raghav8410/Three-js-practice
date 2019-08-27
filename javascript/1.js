f2aba84a668339c5e0ff6d7f870dcd43855dcaec
raghav <raghavgarg8410@gmail.com>
2019-08-27 16:02:10 +0530

    initial commit

--- javascript/1.js
+++ javascript/1.js
@@ -49,40 +49,6 @@ function init(){
 
     scene.add(cube);
 
-    var vertices = [
-    new THREE.Vector3(1,3,1),
-    new THREE.Vector3(1,3,-1),
-    new THREE.Vector3(1,-1,1),
-    new THREE.Vector3(1,-1,-1),
-    new THREE.Vector3(-1,3,-1),
-    new THREE.Vector3(-1,3,1),
-    new THREE.Vector3(-1,-1,-1),
-    new THREE.Vector3(-1,-1,1)
-    ];
-    var faces = [
-    new THREE.Face3(0,2,1),
-    new THREE.Face3(2,3,1),
-    new THREE.Face3(4,6,5),
-    new THREE.Face3(6,7,5),
-    new THREE.Face3(4,5,1),
-    new THREE.Face3(5,0,1),
-    new THREE.Face3(7,6,2),
-    new THREE.Face3(6,3,2),
-    new THREE.Face3(5,7,0),
-    new THREE.Face3(7,2,0),
-    new THREE.Face3(1,3,4),
-    new THREE.Face3(3,6,4),
-    ];
-    var geom = new THREE.Geometry();
-    geom.vertices = vertices;
-    geom.faces = faces;
-    geom.mergeVertices();
-    var goemMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
-
-    var goe = new THREE.Mesh(geom, goemMaterial);
-
-    scene.add(goe);
-
     var sphereGeometry = new THREE.SphereGeometry(4,20,20);
 
     var sphereMaterial = new THREE.MeshLambertMaterial({
